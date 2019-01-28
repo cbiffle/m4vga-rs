77fb162cca2aa39596efbf543f786ede4a9db571
Cliff L. Biffle <code@cliffle.com>
2019-01-27 16:16:54 -0800

    Finish division of ISRs into modules.

--- src/vga/mod.rs
+++ src/vga/mod.rs
@@ -1,6 +1,8 @@
 pub mod rast;
 
 pub mod hstate;
+pub mod bg_rast;
+pub mod shock;
 
 use stm32f4::stm32f407 as device;
 use cortex_m::peripheral as cm;
@@ -42,61 +44,6 @@ static mut DRIVER_INIT_FLAG: AtomicBool = AtomicBool::new(false);
 type WorkingBuffer = [u32; WORKING_BUFFER_SIZE];
 const WORKING_BUFFER_SIZE: usize = rast::TARGET_BUFFER_SIZE / 4;
 
-/// Casting from a word-aligned working buffer to a byte-aligned pixel buffer.
-fn working_buffer_as_u8(words: &mut WorkingBuffer) -> &mut rast::TargetBuffer {
-    // Safety: these structures have exactly the same shape, and when we use it
-    // as a buffer of u32, we're doing it only as a fast way of moving u8. The
-    // main portability risk here is endianness, but so be it.
-    unsafe {
-        core::mem::transmute(words)
-    }
-}
-
-/// State used by the raster maintenance (PendSV) ISR.
-struct RasterState {
-    /// Rasterization working buffer in closely-coupled RAM. During
-    /// rasterization, the CPU can scribble into this buffer freely without
-    /// interfering with any ongoing DMA transfer.
-    working_buffer: &'static mut WorkingBuffer,
-
-    /// Flag indicating that a new scanline has been written to `working_buffer`
-    /// since the last scan buffer update, and so data needs to be copied.
-    update_scan_buffer: bool,
-
-    /// Rasterizer parameters for the contents of `working_buffer`.
-    raster_ctx: RasterCtx,
-}
-
-static mut RASTER_STATE: SpinLock<RasterState> = SpinLock::new(RasterState {
-    working_buffer: unsafe { &mut GLOBAL_WORKING_BUFFER },
-    update_scan_buffer: false,
-    raster_ctx: RasterCtx {
-        cycles_per_pixel: 4,
-        repeat_lines: 0,
-        target_range: 0..0,
-    },
-});
-
-/// Rasterization working buffer in closely-coupled RAM. During rasterization,
-/// the CPU can scribble into this buffer freely without interfering with any
-/// ongoing DMA transfer.
-///
-/// A reference to this is stashed in `RASTER_STATE`; you don't want to touch
-/// this directly. (I'd make it an anonymous array but that would prevent me
-/// from using `link_section`.)
-#[link_section = ".local_ram"]
-static mut GLOBAL_WORKING_BUFFER: WorkingBuffer = [0; WORKING_BUFFER_SIZE];
-
-/// Rasterization scanout buffer in the smaller AHB-attached SRAM. This is the
-/// source for scanout DMA.
-///
-/// Written in PendSV by the `copy_words` routine, which transfers data from
-/// `GLOBAL_WORKING_BUFFER`.
-///
-/// Read asynchronously by DMA during scanout.
-#[link_section = ".scanout_ram"]
-static mut GLOBAL_SCANOUT_BUFFER: WorkingBuffer = [0; WORKING_BUFFER_SIZE];
-
 /// Groups parameters passed from PendSV to HState ISR, describing the next DMA
 /// transfer.
 struct NextTransfer {
@@ -115,21 +62,6 @@ struct NextTransfer {
 static NEXT_XFER: SpinLock<NextTransfer> = SpinLock::new(NextTransfer {
     dma_cr_bits: 0,
     use_timer: false,
-});
-
-/// Description of the last render into the working buffer.
-///
-/// Written:
-/// - At PendSV when rasterization completes.
-///
-/// Read:
-/// - At PendSV to determine how much data to move from working to scanout.
-/// - At PendSV to configure next DMA transfer.
-/// - At PendSV to decide whether to repeat or re-rasterize.
-static LAST_RENDER: SpinLock<RasterCtx> = SpinLock::new(RasterCtx {
-    cycles_per_pixel: 4,
-    repeat_lines: 0,
-    target_range: 0..0,
 });
 
 #[derive(Copy, Clone, Debug)]
@@ -223,12 +155,6 @@ pub enum Ready {}
 static RASTER: rast::IRef = rast::IRef::new();
 
 impl<T> Vga<T> {
-    fn scan_buffer(&self) -> &'static mut [Pixel; rast::TARGET_BUFFER_SIZE] {
-        unsafe {
-            core::mem::transmute(&mut GLOBAL_SCANOUT_BUFFER)
-        }
-    }
-
     /// Busy-waits for the transition from active video to vertical blank.
     /// Because this waits for the *transition*, if you call this *during*
     /// vblank it will wait for an entire frame.
@@ -401,11 +327,16 @@ impl Vga<Idle> {
         }
         */
 
+        /*
+        TODO: soooo I used to do this, but I'm pretty sure the rasterizer code
+        will handle it, so.
+
         // Blank the final word of the scan buffer.
         for b in &mut self.scan_buffer()
                 [timing.video_pixels .. timing.video_pixels + 4] {
             *b = 0
         }
+        */
 
         // Set up global state.
         LINE.store(0, Ordering::Relaxed);
@@ -574,25 +505,6 @@ fn acquire_hw<T: Send>(lock: &SpinLock<Option<T>>) -> SpinLockGuard<T> {
     )
 }
 
-static SHOCK_TIMER: SpinLock<Option<device::TIM3>> = SpinLock::new(None);
-
-/// Entry point for the shock absorber ISR.
-///
-/// The purpose of the shock absorber is to idle the CPU, stopping I/D fetches
-/// and lower-priority interrupts, until a higher-priority interrupt arrives.
-/// This ensures that the interrupt latency of the higher-priority interrupt is
-/// not affected by multi-cycle bus transactions, Flash wait states, or
-/// tail-chaining.
-///
-/// Note: this is `etl_stm32f4xx_tim3_handler` in the C++.
-pub fn shock_absorber_isr() {
-    // Acknowledge IRQ so it doesn't re-occur.
-    acquire_hw(&SHOCK_TIMER)
-        .sr.modify(|_, w| w.cc2if().clear_bit());
-    // Idle the CPU until an interrupt arrives.
-    cortex_m::asm::wfi()
-}
-
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum VState {
     Blank = 0b00,
@@ -626,266 +538,6 @@ fn vert_state() -> VState {
 
 fn set_vert_state(s: VState) {
     VERT_STATE.store(s as usize, Ordering::Relaxed)
-}
-
-/// Entry point for the raster maintenance ISR, invoked as PendSV.
-fn maintain_raster_isr() {
-    // Safety: RASTER_STATE is mut only because rustc is really picky about
-    // seeing uses of mut statics like GLOBAL_WORKING_BUFFER in the initializers
-    // of non-mut statics.
-    let mut state = unsafe { RASTER_STATE.try_lock() }.unwrap();
-
-    let vs = vert_state();
-
-    // First, prepare for scanout from SAV on this line.  This has two purposes:
-    // it frees up the rasterization target buffer so that we can overwrite it,
-    // and it applies pixel timing choices from the *last* rasterizer run to the
-    // scanout machine so that we can replace them as well.
-    //
-    // This writes to the scanout buffer *and* accesses AHB/APB peripherals, so
-    // it *cannot* run concurrently with scanout -- so we do it first, during
-    // hblank.
-    if vs.is_displayed_state() {
-        if state.update_scan_buffer {
-            update_scan_buffer(
-                state.raster_ctx.target_range.end,
-                &mut state.working_buffer, 
-            );
-        }
-        let (dma_cr, use_timer) = {
-            // We need to borrow hardware from the horizontal state machine.
-            // Keep this scope as small as possible to avoid conflict.
-            let hw = acquire_hw(&HSTATE_HW);
-            prepare_for_scanout(
-                &hw.dma2,
-                &hw.tim1,
-                &state.raster_ctx,
-            )
-        };
-        // [OX] TODO: omg there is no actual way to get the bits out of this
-        let dma_cr_bits = unsafe { core::mem::transmute(dma_cr) };
-
-        // Note: we are now racing hstate SAV for control of this lock.
-        *NEXT_XFER.try_lock().unwrap() = NextTransfer {
-            dma_cr_bits, 
-            use_timer,
-        };
-    }
-
-    // Allow the application to do additional work during what's left of hblank.
-    //vga_hblank_interrupt();
-
-    // Second, rasterize the *next* line, if there's a useful next line.
-    // Rasterization can take a while, and may run concurrently with scanout.
-    // As a result, we just stash our results in places where the *next* PendSV
-    // will find and apply them.
-    if (vs.is_rendered_state()) {
-        let state = &mut *state;
-        rasterize_next_line(
-            &*TIMING.try_lock().unwrap().as_mut().unwrap(),
-            &mut state.raster_ctx,
-            &mut state.working_buffer,
-        );
-    }
-}
-
-/// Copy the first `len_bytes` of `working` into the global scanout buffer for
-/// DMA.
-fn update_scan_buffer(len_bytes: usize,
-                      working: &mut WorkingBuffer) {
-    if len_bytes > 0 {
-        // We're going to move words, so round up to find the number of words to
-        // move.
-        //
-        // Note: the user could pass in any value for `len_bytes`, but it will
-        // get bounds-checked below when used as a slice index.
-        let count = (len_bytes + 3) / 4;
-
-        let scan = unsafe { &mut GLOBAL_SCANOUT_BUFFER };
-
-        crate::copy_words::copy_words(
-            &working[..count],
-            &mut scan[..count],
-        );
-
-        // Terminate with a word of black, to ensure that outputs return to
-        // black level for hblank.
-        scan[count] = 0;
-    }
-}
-
-fn rasterize_next_line(timing: &Timing,
-                       ctx: &mut RasterCtx,
-                       working: &mut WorkingBuffer)
-    -> bool
-{
-    let current_line = LINE.load(Ordering::Relaxed);
-    let next_line = current_line + 1;
-    let visible_line = next_line - timing.video_start_line;
-
-    if ctx.repeat_lines == 0 {
-        // Set up a default context for the rasterizer to alter if desired.
-        *ctx = RasterCtx {
-            cycles_per_pixel: timing.cycles_per_pixel(),
-            repeat_lines: 0,
-            target_range: 0..0,
-        };
-        // Invoke the rasterizer.
-        RASTER.observe(|r| r(
-                current_line,
-                working_buffer_as_u8(working),
-                ctx,
-        ));
-        true
-    } else {  // repeat_lines > 0
-        ctx.repeat_lines -= 1;
-        false
-    }
-}
-
-/// Sets up the scanout configuration. This is done well in advance of the
-/// actual start of scanout.
-///
-/// This returns the two pieces of information that are needed to trigger
-/// scanout the rest of the way: a CR value and a flag indicating whether the
-/// scanout will use a timer-generated DRQ (`true`) or run at full speed
-/// (`false`).
-fn prepare_for_scanout(dma: &device::DMA2,
-                       vtimer: &device::tim1::RegisterBlock,
-                       ctx: &RasterCtx)
-    -> (device::dma2::s5cr::W, bool)
-{
-    // Shut off the DMA stream for reconfiguration. This is a little
-    // belt-and-suspenders.
-    dma.s5cr.modify(|_, w| w.en().clear_bit());
-
-    // TODO adjust this
-    const DRQ_SHIFT_CYCLES: u32 = 2;
-
-    // TODO oxidation note: okay, the svd2rust register access operations
-    // are incredibly awkward for code like this. I'm really really tempted
-    // to fork it.
-    //
-    // The issue: I want to create a *dynamic* value to load into the DMA
-    // stream CR register *ahead of time*. And I want to do it without bit
-    // shifting and error-prone offset constants.
-    //
-    // Notes below tagged with [OX].
-
-    // [OX] so we can't have a dma_xfer_common constant like the C++ does,
-    // because *none* of the register manipulation operations are const.
-    // Instead, let's roll one here and hope the compiler figures out how to
-    // optimize it :-(
-
-    // Build the CR value we'll use to start the transfer, so that we don't
-    // have to do it then -- which would increase IRQ-to-transfer latency.
-
-    // [OX] First: all of this code is statically specialized for stream 5.
-    // That's ridiculous. All streams are identical. But look at the type:
-    let mut xfer = device::dma2::s5cr::W::reset_value();
-    // [OX] can't do these alterations in the same line as the declaration
-    // above, because they pass references around instead of W being a
-    // simple value type, and it's thus treated as a temporary and freed.
-    xfer
-        .chsel().bits(6)
-        .pl().very_high()
-        .pburst().single()
-        .mburst().single()
-        .en().enabled();
-
-    let length = ctx.target_range.end - ctx.target_range.start;
-
-    if ctx.cycles_per_pixel > 4 {
-        // Adjust reload frequency of TIM1 to accomodate desired pixel clock.
-        // (ARR value is period - 1.)
-        let reload = (ctx.cycles_per_pixel - 1) as u32;
-        vtimer.arr.write(|w| unsafe {
-            // TODO: ARR unsafe? BUG
-            w.bits(reload)
-        });
-        // Force an update to reset the timer state.
-        vtimer.egr.write(|w| w.ug().set_bit());
-        // Configure the timer as *almost* ready to produce a DRQ, less a small
-        // value (fudge factor).  Gotta do this after the update event, above,
-        // because that clears CNT.
-        vtimer.cnt.write(|w| unsafe {
-            w.bits(reload - DRQ_SHIFT_CYCLES)
-        });
-        vtimer.sr.reset();
-
-        dma.s5par.write(|w| unsafe {
-            // Okay, this is legitimately unsafe. ;-)
-            w.bits(0x40021015)  // High byte of GPIOE ODR (hack hack)
-        });
-        dma.s5m0ar.write(|w| unsafe {
-            w.bits(&GLOBAL_SCANOUT_BUFFER as *const u32 as u32)
-        });
-
-        // The number of bytes read must exactly match the number of bytes
-        // written, or the DMA controller will freak out.  Thus, we must adapt
-        // the transfer size to the number of bytes transferred. The padding in
-        // each case is because we want to send (at least) one byte of black
-        // after any scanline.
-        match length & 3 {
-            0 => {
-                xfer.msize().word();
-                dma.s5ndtr.write(|w| w.ndt().bits(length as u16 + 4));
-            }
-            2 => {
-                xfer.msize().half_word();
-                dma.s5ndtr.write(|w| w.ndt().bits(length as u16 + 2));
-            }
-            _ => {
-                xfer.msize().byte();
-                dma.s5ndtr.write(|w| w.ndt().bits(length as u16 + 1));
-            }
-        }
-
-        xfer.dir().memory_to_peripheral()
-            .minc().clear_bit()
-            .psize().byte()
-            .pinc().clear_bit();
-       
-        (xfer, true)
-    } else {
-        // Note that we're using memory as the peripheral side.
-        // This DMA controller is a little odd.
-        dma.s5par.write(|w| unsafe {
-            w.bits(&GLOBAL_SCANOUT_BUFFER as *const u32 as u32)
-        });
-        dma.s5m0ar.write(|w| unsafe {
-            // Okay, this is legitimately unsafe. ;-)
-            w.bits(0x40021015)  // High byte of GPIOE ODR (hack hack)
-        });
-
-        // The number of bytes read must exactly match the number of bytes
-        // written, or the DMA controller will freak out.  Thus, we must adapt
-        // the transfer size to the number of bytes transferred. The padding in
-        // each case is because we want to send (at least) one byte of black
-        // after any scanline.
-        match length & 3 {
-            0 => {
-                xfer.psize().word();
-                dma.s5ndtr.write(|w| w.ndt().bits(length as u16 / 4 + 1));
-            }
-            2 => {
-                xfer.psize().half_word();
-                dma.s5ndtr.write(|w| w.ndt().bits(length as u16 / 2 + 1));
-            }
-            _ => {
-                xfer.psize().byte();
-                dma.s5ndtr.write(|w| w.ndt().bits(length as u16 + 1));
-            }
-        }
-
-        xfer
-            .dir().memory_to_memory()
-            .pinc().set_bit()
-            .msize().byte()
-            .minc().set_bit();
-
-        (xfer, false)
-    }
 }
 
 fn disable_h_timer(nvic: &mut cortex_m::peripheral::NVIC,
---
+++ src/vga/shock.rs
@@ -0,0 +1,34 @@
+//! The "shock absorber" ISR.
+//!
+//! This exists to minimize jitter in the latency between our start-of-active
+//! timer going off, and the hstate ISR executing. In the wild, this latency is
+//! affected by...
+//!
+//! 1. Wait-stated bus transactions, particularly fetches from Flash.
+//! 2. Multi-word burst transactions, such as unaligned accesses or bitband
+//!    writes.
+//! 3. Tail chaining -- if the timer goes off near the end of another ISR, the
+//!    processor will jump directly from one to the other, *reducing* latency.
+//!
+//! We work around this with the shock absorber. Its job is to fire a few cycles
+//! before we expect the actual interrupt, and idle the CPU. This ensures that
+//! the CPU and bus are quiet when the interrupt fires.
+
+use stm32f4::stm32f407 as device;
+use crate::vga::{acquire_hw};
+use crate::util::spin_lock::SpinLock;
+
+static SHOCK_TIMER: SpinLock<Option<device::TIM3>> = SpinLock::new(None);
+
+/// Entry point for the shock absorber ISR.
+///
+/// Note: this is `etl_stm32f4xx_tim3_handler` in the C++.
+pub fn shock_absorber_isr() {
+    // Acknowledge IRQ so it doesn't re-occur.
+    acquire_hw(&SHOCK_TIMER)
+        .sr.modify(|_, w| w.cc2if().clear_bit());
+    // Idle the CPU until an interrupt arrives.
+    cortex_m::asm::wfi()
+}
+
+
