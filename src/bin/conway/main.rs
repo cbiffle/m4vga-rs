cccf6b66f7f0c99d31bb1462257578e11c0d46ad
Cliff L. Biffle <code@cliffle.com>
2019-01-29 20:19:33 -0800

    conway: tolerate preemption in rasterizer
    This hit *incredibly awkward* timing where the rasterizer fg lock
    routine ran *just* as SAV was happening, causing the lock to fail.
    (I had thought it was immune to preemption, but I was confused.)

--- src/bin/conway/main.rs
+++ src/bin/conway/main.rs
@@ -14,7 +14,7 @@ use core::sync::atomic::AtomicUsize;
 use stm32f4;
 
 use stm32f4::stm32f407::interrupt;
-use m4vga::util::rw_lock::ReadWriteLock;
+use m4vga::util::rw_lock::{ReadWriteLock, TryLockError};
 
 // this can go in the default SRAM
 static mut BUF0: [u32; 800 * 600 / 32] = [0; 800*600/32];
@@ -40,7 +40,15 @@ fn main() -> ! {
             |ln, tgt, ctx| {
                 #[cfg(feature = "measurement")]
                 m4vga::measurement::sig_d_set();
-                let fg = fg.try_lock().unwrap();
+
+                let fg = loop {
+                    match fg.try_lock() {
+                        Ok(fg) => break fg,
+                        Err(TryLockError::Race) => continue,
+                        Err(_) => panic!("fg unavailable"),
+                    }
+                };
+
                 #[cfg(feature = "measurement")]
                 m4vga::measurement::sig_d_clear();
 
