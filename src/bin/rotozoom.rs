41fc3f986f82a0ccfa94e599cc2e27d9b13e27b5
Cliff L. Biffle <code@cliffle.com>
2019-02-03 10:40:45 -0800

    rotozoom: add RaceBuffer for checked beam racing
    The RaceBuffer construct is something I'd like to extract and make
    general. It is effectively a single-producer single-consumer queue of
    scanlines. I like the result better than the previous coarse banded
    approach.
    
    Performance is down by 0.1%. I'll take it.

--- src/bin/rotozoom.rs
+++ src/bin/rotozoom.rs
@@ -8,11 +8,11 @@ extern crate panic_itm;
 #[cfg(feature = "panic-halt")]
 extern crate panic_halt;
 
+use core::sync::atomic::{AtomicUsize, Ordering};
 use stm32f4;
 use stm32f4::stm32f407::interrupt;
 
 use m4vga::rast::direct;
-use m4vga::util::spin_lock::SpinLock;
 use m4vga::math::{Mat3f, Vec2};
 
 use libm::F32Ext;
@@ -27,17 +27,29 @@ const BUFFER_SIZE: usize = WIDTH * HALF_HEIGHT;
 const BUFFER_WORDS: usize = BUFFER_SIZE / 4;
 const BUFFER_STRIDE: usize = WIDTH / 4;
 
-static mut TOP: [u32; BUFFER_WORDS] = [0; BUFFER_WORDS];
-#[link_section = ".local_bss"]
-static mut BOT: [u32; BUFFER_WORDS] = [0; BUFFER_WORDS];
-
 /// Demo entry point. Responsible for starting up the display driver and
 /// providing callbacks.
 #[allow(unused_parens)] // TODO bug in cortex_m_rt
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let top = SpinLock::new(unsafe { &mut TOP });
-    let bot = SpinLock::new(unsafe { &mut BOT });
+    // Safety: as long as these are the *only* mutable references we produce to
+    // those statics, we're good.
+    let mut buf = RaceBuffer::new([
+        {
+            static mut TOP: [u32; BUFFER_WORDS] = [0; BUFFER_WORDS];
+            // Safety: because of scoping this is clearly the only mutable
+            // reference we generate to this static.
+            unsafe { &mut TOP }
+        },
+        {
+            #[link_section = ".local_bss"]
+            static mut BOT: [u32; BUFFER_WORDS] = [0; BUFFER_WORDS];
+            // Safety: because of scoping this is clearly the only mutable
+            // reference we generate to this static.
+            unsafe { &mut BOT }
+        },
+    ]);
+    let (mut reader, mut writer) = buf.split();
 
     // Give the driver its hardware resources...
     m4vga::take_hardware()
@@ -48,15 +60,10 @@ fn main() -> ! {
             // The raster callback is invoked on every horizontal retrace to
             // provide new pixels. Here, we just scribble a test pattern into
             // the target buffer.
-            |ln, tgt, ctx| {
-                let ln = ln / Y_SCALE;
-                let (buf, ln) = if ln < HALF_HEIGHT {
-                    (unsafe { &mut TOP }, ln)
-                } else {
-                    (unsafe { &mut BOT }, ln - HALF_HEIGHT)
-                };
+            |_, tgt, ctx| {
+                let buf = reader.take_line();
                 direct::direct_color(
-                    ln,
+                    0,
                     tgt,
                     ctx,
                     buf,
@@ -81,9 +88,6 @@ fn main() -> ! {
                 let rot = Mat3f::rotate(ROT);
 
                 loop {
-                    vga.sync_to_vblank();
-                    m4vga::measurement::sig_d_set();
-
                     let s = (frame as f32 / 50.).sin() * 0.7 + 1.;
                     let tx = (frame as f32 / 100.).cos() * 100.;
                     let ty = 0.;
@@ -100,40 +104,27 @@ fn main() -> ! {
                     let xi = (top_right - top_left) * (1. / cols);
                     let yi = (bot_left - top_left) * (1. / rows);
                     let mut ybase = top_left;
-                    let mut buf = top.try_lock().expect("render access");
-                    let bg = u32_as_u8_mut(&mut **buf);
-                    for y in 0..HALF_HEIGHT {
+                    m4vga::measurement::sig_d_set();
+                    for _ in 0..HEIGHT {
+                        let mut buf = writer.generate_line();
+                        let buf = u32_as_u8_mut(&mut *buf);
                         {
                             let mut pos = ybase;
                             for x in 0..WIDTH {
-                                bg[y * WIDTH + x] =
-                                    tex_fetch(pos.0[0], pos.0[1]) as u8;
+                                buf[x] = tex_fetch(pos.0[0], pos.0[1]) as u8;
                                 pos = pos + xi;
                             }
                         }
                         ybase = ybase + yi
                     }
-                    drop(bg); drop(buf);
 
-                    let mut buf = bot.try_lock().expect("render access");
-                    let bg = u32_as_u8_mut(&mut **buf);
-                    for y in 0..HALF_HEIGHT {
-                        {
-                            let mut pos = ybase;
-                            for x in 0..WIDTH {
-                                bg[y * WIDTH + x] =
-                                    tex_fetch(pos.0[0], pos.0[1]) as u8;
-                                pos = pos + xi;
-                            }
-                        }
-                        ybase = ybase + yi
-                    }
-                    drop(bg); drop(buf);
                     m = m * rot;
 
                     frame += 1;
 
                     m4vga::measurement::sig_d_clear();
+                    vga.sync_to_vblank();
+                    writer.reset();
                 }
             })
 }
@@ -148,6 +139,111 @@ fn u32_as_u8_mut(slice: &mut [u32]) -> &mut [u8] {
             slice.as_mut_ptr() as *mut u8,
             slice.len() * 4,
         )
+    }
+}
+
+/// A specialized framebuffer structure with two features:
+///
+/// 1. The framebuffer is split into two parts, because the whole thing won't
+///    fit into any single RAM.
+/// 2. It checks for aliasing on a scanline granularity so rendering can race
+///    scanout more aggressively.
+struct RaceBuffer {
+    segments: [&'static mut [u32; BUFFER_WORDS]; 2],
+    rendered: AtomicUsize,
+    displayed: AtomicUsize,
+}
+
+impl RaceBuffer {
+    pub fn new(segments: [&'static mut [u32; BUFFER_WORDS]; 2]) -> Self {
+        RaceBuffer {
+            segments,
+            rendered: 0.into(),
+            displayed: 0.into(),
+        }
+    }
+
+    pub fn split(&mut self) -> (RaceReader, RaceWriter) {
+        let alias = unsafe { & *(self as *const _) };
+        (
+            RaceReader { buf: alias },
+            RaceWriter { buf: self },
+        )
+    }
+}
+
+struct RaceReader<'a> {
+    buf: &'a RaceBuffer,
+}
+
+impl<'a> RaceReader<'a> {
+    pub fn take_line(&mut self) -> &[u32] {
+        let line_number = self.buf.displayed.fetch_add(1, Ordering::Relaxed);
+        let rendered = self.buf.rendered.load(Ordering::Relaxed);
+        let gap = rendered.wrapping_sub(line_number);
+
+        if gap <= HEIGHT {
+            let (seg_index, line_number) = if line_number < HALF_HEIGHT {
+                (0, line_number)
+            } else {
+                (1, line_number - HALF_HEIGHT)
+            };
+            let offset = line_number * BUFFER_STRIDE;
+            &self.buf.segments[seg_index][offset .. offset + BUFFER_STRIDE]
+        } else {
+            panic!("tearing: scanout reached {} but rendering only {}",
+                   line_number, rendered);
+        }
+    }
+}
+
+struct RaceWriter<'a> {
+    buf: &'a mut RaceBuffer,
+}
+
+impl<'a> RaceWriter<'a> {
+    pub fn generate_line(&mut self) -> GenGuard {
+        let line_number = self.buf.rendered.load(Ordering::Relaxed);
+        let (seg_index, line_number) = if line_number < HALF_HEIGHT {
+            (0, line_number)
+        } else {
+            (1, line_number - HALF_HEIGHT)
+        };
+        let offset = line_number * BUFFER_STRIDE;
+        GenGuard {
+            counter: &self.buf.rendered,
+            data: &mut self.buf.segments[seg_index]
+                [offset .. offset + BUFFER_STRIDE],
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.buf.rendered.store(0, Ordering::Relaxed);
+        self.buf.displayed.store(0, Ordering::Relaxed);
+    }
+}
+
+struct GenGuard<'a> {
+    counter: &'a AtomicUsize,
+    data: &'a mut [u32],
+}
+
+impl<'a> Drop for GenGuard<'a> {
+    fn drop(&mut self) {
+        self.counter.fetch_add(1, Ordering::Relaxed);
+    }
+}
+
+impl<'a> core::ops::Deref for GenGuard<'a> {
+    type Target = [u32];
+    fn deref(&self) -> &Self::Target {
+        self.data
+    }
+}
+
+impl<'a> core::ops::DerefMut for GenGuard<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.data
     }
 }
 
