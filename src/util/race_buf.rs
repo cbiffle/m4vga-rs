3d2003731dd8ae69b02901e3bb1f0d032a848700
Cliff L. Biffle <code@cliffle.com>
2019-02-07 21:16:21 -0800

    Improve docs on RaceBuffer

--- src/util/race_buf.rs
+++ src/util/race_buf.rs
@@ -10,12 +10,29 @@ use crate::priority;
 ///    fit into any single RAM.
 /// 2. It checks for aliasing on a scanline granularity so rendering can race
 ///    scanout more aggressively.
+///
+/// # Use of priority tokens
+///
+/// Users of a `RaceBuffer` must provide *priority tokens* to most API calls.
+/// Specifically,
+///
+/// - Scanlines can only be *written* from thread mode, outside of any
+///   interrupt.
+/// - Scanlines can only be *read* from interrupt handlers.
+/// - A scanline accessed in an interrupt handler cannot be stored outside the
+///   execution of that interrupt handler.
+///
+/// This simplifies the implementation, by ensuring that read operations cannot
+/// be preempted by write operations.
 pub struct RaceBuffer<R: 'static> {
     segments: [&'static mut [R]; 2],
     write_mark: AtomicUsize,
 }
 
 impl<R: 'static> RaceBuffer<R> {
+    /// Creates a `RaceBuffer` from two static bands of rows.
+    ///
+    /// The bands need not be the same length.
     pub fn new(segments: [&'static mut [R]; 2])
         -> Self
     {
@@ -25,6 +42,8 @@ impl<R: 'static> RaceBuffer<R> {
         }
     }
 
+    /// Generates a `RaceReader` and `RaceWriter` for this buffer, which can
+    /// then be distributed to the renderer and rasterizer.
     pub fn split(&mut self) -> (RaceReader<R>, RaceWriter<R>) {
         (
             RaceReader {
@@ -41,6 +60,7 @@ impl<R: 'static> RaceBuffer<R> {
     }
 }
 
+/// Pulls rendered scanlines from a `RaceBuffer`.
 pub struct RaceReader<'a, R: 'static> {
     buf: NonNull<RaceBuffer<R>>,
     _life: PhantomData<&'a ()>,
@@ -57,6 +77,15 @@ impl<'a, R: 'static> RaceReader<'a, R> {
         unsafe { &self.buf.as_ref().segments[0] }.len()
     }
 
+    /// Gets a reference to a scanline, identified by `line_number`.
+    ///
+    /// If the renderer has not finished with this scanline, we have found a
+    /// dynamic data race; `take_line` will `panic`.
+    ///
+    /// The caller is required to provide an interrupt priority token `P`,
+    /// proving that they are calling from interrupt context. This ensures that
+    /// this operation is atomic with respect to the `RaceWriter`, which must be
+    /// used *outside* an interrupt.
     pub fn take_line<'r, P>(&'r mut self,
                             line_number: usize,
                             _: &'r P) -> &'r R
@@ -83,6 +112,7 @@ impl<'a, R: 'static> RaceReader<'a, R> {
     }
 }
 
+/// Vends unrendered scanlines and tracks when they're completed.
 pub struct RaceWriter<'a, R: 'static> {
     buf: NonNull<RaceBuffer<R>>,
     _life: PhantomData<&'a ()>,
@@ -97,6 +127,19 @@ impl<'a, R: 'static> RaceWriter<'a, R> {
         unsafe { &self.buf.as_ref().segments[0] }.len()
     }
 
+    /// Gets the next scanline for rendering.
+    ///
+    /// The scanline is returned as a `GenGuard` smart pointer. This works like
+    /// a `&mut`, and will mark the scanline as completed when it's dropped.
+    ///
+    /// The caller is required to provide a `Thread` priority token,
+    /// demonstrating that they are *not* attempting to use this API from an
+    /// ISR. This allows the implementation to be somewhat simpler.
+    ///
+    /// # Panics
+    ///
+    /// If the next scanline would run off the end of the final framebuffer
+    /// band.
     pub fn generate_line(&mut self,
                          _: &priority::Thread) -> GenGuard<R> {
         let line_number = self.load_writer_progress();
@@ -110,9 +153,21 @@ impl<'a, R: 'static> RaceWriter<'a, R> {
         GenGuard {
             counter: &buf.write_mark,
             data: &mut buf.segments[seg_index][line_number],
+            _not_sync_send: PhantomData,
         }
     }
 
+    /// Resets the buffer to empty to begin a new frame.
+    ///
+    /// After this call,
+    ///
+    /// - The next line handed out by `generate_line` will be scanline zero.
+    /// - No lines will be available to the `RaceReader`.
+    ///
+    /// The caller is required to provide a `Thread` priority token, which
+    /// proves that this call is not executing concurrently with the
+    /// `RaceReader` (which can only be used from ISRs). This simplifies the
+    /// implementation.
     pub fn reset(&mut self, _: &priority::Thread) {
         unsafe { self.buf.as_ref() }.write_mark.store(0, Ordering::Relaxed)
     }
@@ -121,6 +176,9 @@ impl<'a, R: 'static> RaceWriter<'a, R> {
 pub struct GenGuard<'a, R> {
     counter: &'a AtomicUsize,
     data: &'a mut R,
+    /// Conservatively prevent this smart pointer from being moved into an ISR,
+    /// because I haven't thought through the implications of doing so.
+    _not_sync_send: PhantomData<*mut R>,
 }
 
 impl<'a, R> Drop for GenGuard<'a, R> {
