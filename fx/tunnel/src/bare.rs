a21bdcdb6c0cf5720188399761e08429e6fcc0e6
Cliff L. Biffle <code@cliffle.com>
2019-06-01 08:38:31 -0700

    fx/tunnel: make more code platform-independent

--- fx/tunnel/src/bare.rs
+++ fx/tunnel/src/bare.rs
@@ -17,20 +17,20 @@ static mut BUF1: [u32; super::BUFFER_WORDS] = [0; super::BUFFER_WORDS];
 static mut TABLE: table::Table =
     [[table::Entry::zero(); table::TAB_WIDTH]; table::TAB_HEIGHT];
 
-pub struct State {
-    fg: SpinLock<&'static mut [u32; super::BUFFER_WORDS]>,
-    bg: Option<&'static mut [u32; super::BUFFER_WORDS]>,
-    table: &'static table::Table,
+pub struct State<'d> {
+    fg: SpinLock<&'d mut [u32; super::BUFFER_WORDS]>,
+    bg: Option<&'d mut [u32; super::BUFFER_WORDS]>,
+    table: &'d table::Table,
 }
 
-pub struct RasterState<'a> {
-    fg: &'a SpinLock<&'static mut [u32; super::BUFFER_WORDS]>,
+pub struct RasterState<'a, 'd> {
+    fg: &'a SpinLock<&'d mut [u32; super::BUFFER_WORDS]>,
 }
 
-pub struct RenderState<'a> {
-    fg: &'a SpinLock<&'static mut [u32; super::BUFFER_WORDS]>,
-    bg: &'static mut [u32; super::BUFFER_WORDS],
-    table: &'static table::Table,
+pub struct RenderState<'a, 'd> {
+    fg: &'a SpinLock<&'d mut [u32; super::BUFFER_WORDS]>,
+    bg: &'d mut [u32; super::BUFFER_WORDS],
+    table: &'d table::Table,
 }
 
 /// Initializes a `State` from static context.
@@ -38,7 +38,7 @@ pub struct RenderState<'a> {
 /// # Safety
 ///
 /// This is safe as long as it's only called once.
-pub unsafe fn init() -> State {
+pub unsafe fn init() -> State<'static> {
     let table = &mut TABLE;
     table::compute(table);
     let table = &*table;
@@ -49,8 +49,8 @@ pub unsafe fn init() -> State {
     State { fg, bg, table }
 }
 
-impl State {
-    pub fn split(&mut self) -> (RasterState, RenderState) {
+impl<'d> State<'d> {
+    pub fn split(&mut self) -> (RasterState<'_, 'd>, RenderState<'_, 'd>) {
         (
             RasterState {
                 fg: &self.fg,
@@ -64,7 +64,7 @@ impl State {
     }
 }
 
-impl<'a> RasterState<'a> {
+impl<'a, 'd> RasterState<'a, 'd> {
     pub fn raster_callback(
         &mut self,
         ln: usize,
@@ -72,53 +72,29 @@ impl<'a> RasterState<'a> {
         ctx: &mut m4vga::rast::RasterCtx,
         _: m4vga::priority::I0,
     ) {
-        // Our image is slightly smaller than the display. Black the top and
-        // bottom borders.
-        if ln < 4 || ln > 595 {
-            m4vga::rast::solid_color_fill(target, ctx, 800, 0);
-            return;
-        }
-
-        let buf = self.fg.try_lock().expect("rast fg access");
-
-        let ln = ln / super::SCALE;
-
-        if ln < super::HALF_HEIGHT {
-            m4vga::rast::direct::direct_color(ln, target, ctx, *buf, super::BUFFER_STRIDE);
-        } else {
-            m4vga::rast::direct::direct_color_mirror(ln, target, ctx, *buf, super::BUFFER_STRIDE, super::HEIGHT);
-        }
-
-        ctx.cycles_per_pixel *= super::SCALE;
-        ctx.repeat_lines = super::SCALE - 1;
+        super::raster_callback(
+            ln,
+            target,
+            ctx,
+            self.fg,
+        )
     }
         
 }
 
-impl<'a> RenderState<'a> {
+impl<'a, 'd> RenderState<'a, 'd> {
     pub fn render_loop(&mut self, vga: &mut m4vga::Vga<m4vga::Live>) -> ! {
         vga.video_on();
         let mut frame = 0;
         loop {
             vga.sync_to_vblank();
-            core::mem::swap(
+            super::render_frame(
                 &mut self.bg,
-                &mut *self.fg.try_lock().expect("swap access"),
+                self.fg,
+                self.table,
+                frame,
             );
-            let bg = u32_as_u8_mut(self.bg);
-            m4vga::util::measurement::sig_d_set();
-            super::render::render(self.table, bg, frame);
-            m4vga::util::measurement::sig_d_clear();
             frame = (frame + 1) % 65536;
         }
-    }
-}
-
-fn u32_as_u8_mut(slice: &mut [u32]) -> &mut [u8] {
-    unsafe {
-        core::slice::from_raw_parts_mut(
-            slice.as_mut_ptr() as *mut u8,
-            slice.len() * 4,
-        )
     }
 }
--- fx/tunnel/src/lib.rs
+++ fx/tunnel/src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use m4vga::util::spin_lock::SpinLock;
+
 pub mod table;
 pub mod render;
 
@@ -20,3 +22,55 @@ pub const BUFFER_STRIDE: usize = WIDTH / 4;
 mod bare;
 #[cfg(target_os = "none")]
 pub use bare::*;
+
+pub fn raster_callback(
+    ln: usize,
+    target: &mut m4vga::rast::TargetBuffer,
+    ctx: &mut m4vga::rast::RasterCtx,
+    fg: &SpinLock<&mut [u32; BUFFER_WORDS]>,
+) {
+    // Our image is slightly smaller than the display. Black the top and
+    // bottom borders.
+    if ln < 4 || ln > 595 {
+        m4vga::rast::solid_color_fill(target, ctx, 800, 0);
+        return;
+    }
+
+    let buf = fg.try_lock().expect("rast fg access");
+
+    let ln = ln / SCALE;
+
+    if ln < HALF_HEIGHT {
+        m4vga::rast::direct::direct_color(ln, target, ctx, *buf, BUFFER_STRIDE);
+    } else {
+        m4vga::rast::direct::direct_color_mirror(ln, target, ctx, *buf, BUFFER_STRIDE, HEIGHT);
+    }
+
+    ctx.cycles_per_pixel *= SCALE;
+    ctx.repeat_lines = SCALE - 1;
+}
+
+pub fn render_frame<'buf>(
+    bg: &mut &'buf mut [u32; BUFFER_WORDS],
+    fg: &SpinLock<&'buf mut [u32; BUFFER_WORDS]>,
+    table: &table::Table,
+    frame: usize,
+) {
+    core::mem::swap(
+        bg,
+        &mut *fg.try_lock().expect("swap access"),
+    );
+    let bg = u32_as_u8_mut(*bg);
+    m4vga::util::measurement::sig_d_set();
+    self::render::render(table, bg, frame);
+    m4vga::util::measurement::sig_d_clear();
+}
+
+fn u32_as_u8_mut(slice: &mut [u32]) -> &mut [u8] {
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            slice.as_mut_ptr() as *mut u8,
+            slice.len() * 4,
+        )
+    }
+}
